95cdb33312829bb1c6593dbb27a442553fbe0703
AndresFelipePadillaHurtado <afpadilla84@misena.edu.co>
2021-12-18 20:35:17 -0500

    Flameshot - Screenshot

--- autostart.sh
+++ autostart.sh
@@ -13,3 +13,11 @@ cbatticon &
 # Restaurar Wallpaper
 nitrogen --restore &
 
+# Iniciar el sonido
+alsactl init &
+
+# Montar Partcion compartida Developer
+mount -t ntfs-3g -o rdwo /dev/sda5 Developer/ &
+
+# Screen
+flameshot & 
